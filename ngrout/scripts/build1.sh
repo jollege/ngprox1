a56a7195d9ea31866bf84e1c0cb1ccf63cc63c3d
Jonas Ellehauge <ellvtr>
2017-11-02 13:35:56 +0100

    Frustrating: Tried many different config options, cannot make nginx proxy to :8080

--- ngrout/scripts/build1.sh
+++ ngrout/scripts/build1.sh
@@ -3,11 +3,10 @@ echo "mkdir /etc/nginx/sites-enabled"
 mkdir /etc/nginx/sites-enabled
 
 echo "Game 2048" > /usr/share/nginx/game2048/index.html
-echo "cat /usr/share/nginx/game2048/index.html"
-cat /usr/share/nginx/game2048/index.html
+# echo "cat /usr/share/nginx/game2048/index.html"
+# cat /usr/share/nginx/game2048/index.html
 
-# ln -s /etc/nginx/sites-available/ellehauge.net /etc/nginx/sites-enabled/ellehauge.net
-ln -s /etc/nginx/sites-available/game2048 /etc/nginx/sites-enabled/game2048
+# ln -s /etc/nginx/sites-available/game2048 /etc/nginx/sites-enabled/game2048
 
 echo "ls /etc/nginx/sites-available"
 ls -lAh /etc/nginx/sites-available
