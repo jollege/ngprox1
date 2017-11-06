3a20a7955a1aa6370d595a36f574b4297a34b838
jollege <jollege>
2017-11-06 10:42:33 +0100

    Different image names for local and prod environment.

--- ngrout/build.sh
+++ ngrout/build.sh
@@ -3,5 +3,12 @@ if [ -z $1 ]; then
   echo "specify host as \$1"
   exit 1;
 fi
-docker build -t ellvtr/ngrout --build-arg host=$1 .
+
+if [ $1 = "local" ]; then
+  img_name=ellvtr/ngrout_local
+else
+  img_name=ellvtr/ngrout
+fi
+
+docker build -t $img_name --build-arg host=$1 .
 # docker build -t ellvtr/ngrout .
