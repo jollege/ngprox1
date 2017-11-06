41265f06438560a6027f3051cc5efae4264a5cb1
jollege <jollege>
2017-11-06 14:49:24 +0100

    New image "ellvtr/nginx-php" is base image for the awesomemap.tools site. Improved ngrout/build.sh to exit if wrong argument.

--- ngrout/build.sh
+++ ngrout/build.sh
@@ -1,14 +1,17 @@
 # Specify host "local" or "dig" as 1st arg $1:
 if [ -z $1 ]; then
-  echo "specify host as \$1"
-  exit 1;
+  echo "specify host as \$1"; exit 1;
 fi
 
+# Se image name:
 if [ $1 = "local" ]; then
   img_name=ellvtr/ngrout_local
-else
+elif [ $1 = "dig" ]; then
   img_name=ellvtr/ngrout
+else
+  echo "wrong value for \$1"; exit 1;
 fi
 
+# Build docker image:
 docker build -t $img_name --build-arg host=$1 .
 # docker build -t ellvtr/ngrout .
