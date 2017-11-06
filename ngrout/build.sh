1656e118aa8730bc65cf615b72191b612bdc0e77
jollege <jollege>
2017-11-06 10:36:08 +0100

    Now builds different image for env 'local' and 'dig' (digitalocean).

--- ngrout/build.sh
+++ ngrout/build.sh
@@ -1,1 +1,7 @@
-docker build -t ellvtr/ngrout .
+# Specify host "local" or "dig" as 1st arg $1:
+if [ -z $1 ]; then
+  echo "specify host as \$1"
+  exit 1;
+fi
+docker build -t ellvtr/ngrout --build-arg host=$1 .
+# docker build -t ellvtr/ngrout .
--- ngrout/scripts/build1.sh
+++ ngrout/scripts/build1.sh
@@ -1,16 +1,30 @@
-echo "hostname -i"
-hostname -i
+# echo "env 1st arg = $1"
+# echo "env HOST = $HOST"
+export _host=$1
+
+if [ -z "$1" ]; then
+  echo "no host specified with \$1, try \$HOST"
+  export _host=$HOST
+fi
+
+if [ -z "$_host" ]; then
+  echo "error; no host at all, exit"
+  exit 1;
+fi
+
+echo "_host = $_host"
 
 echo "mkdir /etc/nginx/sites-enabled"
 mkdir /etc/nginx/sites-enabled
 
-# echo "Game 2048" > /usr/share/nginx/game2048/index.html
-# echo "cat /usr/share/nginx/game2048/index.html"
-# cat /usr/share/nginx/game2048/index.html
-
-ln -s /etc/nginx/sites-available/app2 /etc/nginx/sites-enabled/app2
-# ln -s /etc/nginx/sites-available/app1 /etc/nginx/sites-enabled/app1
-# ln -s /etc/nginx/sites-available/game2048 /etc/nginx/sites-enabled/game2048
+echo "Setting symlink to active configuration"
+if [ "$_host" = "local" ]; then
+  echo "active config for local"
+  ln -s /etc/nginx/sites-available/app2 /etc/nginx/sites-enabled/app2
+elif [ "$_host" = "dig" ]; then
+  echo "active config for digitalocean1"
+  ln -s /etc/nginx/sites-available/digitalocean1 /etc/nginx/sites-enabled/digitalocean1
+fi
 
 echo "ls /etc/nginx/sites-available"
 ls -lAh /etc/nginx/sites-available
