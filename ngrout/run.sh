f94db901de045f8c6446878095549823558bc130
jollege <jollege>
2017-11-08 16:03:36 +0100

    New domain and service awe1.awesomemap.tools added to nginx reverse proxy.

--- ngrout/run.sh
+++ ngrout/run.sh
@@ -4,5 +4,9 @@ docker run --name game2048 -p 8080:8080 -d ellvtr/game2048a
 docker rm -f game
 docker run --name game -p 9999:8080 -d ellvtr/game2048a
 
+docker rm -f awe1
+docker run --name awe1 -p 9998:8080 -d ellvtr/awemap01
+
 docker rm -f ngrout
-docker run --name ngrout --link=game2048 --link=game -p 80:80 -d ellvtr/ngrout
+docker run --name ngrout --link=game2048 --link=game --link=awe1\
+  -p 80:80 -d ellvtr/ngrout_local
